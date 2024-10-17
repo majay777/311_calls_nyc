ffdd5060a7c56c06090aab1d7b87416e7013b09c
majay777 <ajaymugle87@gmail.com>
2024-10-17 22:18:50 +0530

    new

---
+++ dagster_files/dagster_files/assets/constants.py
@@ -0,0 +1,12 @@
+import os
+
+
+def get_path(path:str) -> str:
+    return path
+
+
+calls_file_path = get_path(os.path.join("data","raw", "call_records_{}.parquet"))
+DATE_FORMAT = "%Y-%m-%d"
+
+START_DATE = "2023-01-01"
+END_DATE = "2023-04-01"
